ca1a29db53063799e428cf2e046fdbe9e2c0a144
Javier <javiergonzalez74@hotmail.com>
2023-01-16 21:08:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
+
+
+app.get('/', (req, res, next) => {    
+    res.render("home", {
+        
+    });
+})
+
+app.get('/about', (req, res, next) => {    
+    res.render('about')
+})
+
+app.get('/works', (req, res, next) => {
+    res.render('works')
+})
+
+
+const port = 3000
+app.listen(port, () => console.log(`Application running at por ${port}`))
